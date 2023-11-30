6ba195696115cfdf5cb0865f511d8c2749dfbdfd
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2023-11-30 19:32:39 +0200

    .

--- test.sh
+++ test.sh
@@ -9,7 +9,6 @@ exec 4<"$TMPFILE"
 
 print_menu () {
     echo 
-    echo "Select one of the following options by typing the respective number: "
     echo "Select business file - - - - - - -(1)"
     echo "Print business data - - - - - - - (2)"
     echo "Update business data field - - - -(3)"
@@ -17,6 +16,7 @@ print_menu () {
     echo "Save changes - - - - - - - - - - -(5)"
     echo "Exit - - - - - - - - - - - - - - -(6)"
     echo
+    echo -n "Select one of the options above by typing the respective number: "
 }
 
 check_file () {
@@ -28,86 +28,111 @@ check_file () {
     fi
 }
 
+select_file () {
+    echo -n "Enter the path to your selected business file: "
+    read FILE_LOCATION
+    if test -f "$FILE_LOCATION"; then
+        echo
+        echo "File selected!"
+        FILE=$FILE_LOCATION
+    else
+        echo
+        echo "File does not exist. Default file selected."
+        FILE="./Businesses.csv"
+    fi
+    cp $FILE $TMPFILE
+}
+
+print_business_data () {
+    echo -n "Enter business code: "
+    read BUSINESS_CODE
+    echo
+    awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
+    echo
+}
+
+update_data () {
+    echo -n "Enter business code: "
+    read BUSINESS_CODE
+    awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE > tmp
+    if [[ -s tmp ]]; then
+        echo -n "Enter which field you would like to change: "
+        read FIELD_INDEX
+
+        echo "Old data:"
+        echo
+        awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
+        echo
+        awk -v x="$BUSINESS_CODE" 'BEGIN {FS=OFS=","} $1==x {$2="Starbucks"} 1' $TMPFILE > tmp && mv tmp $TMPFILE
+        echo "New data:"
+        echo
+        awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
+    else
+        echo "Business code not found."
+    fi
+    rm -f tmp
+    echo
+}
+
+print_entire_file () {
+    echo
+    cat $TMPFILE | column -s"," -t | more -df
+}
+
+save_changes () {
+    echo -n "Are you sure to want to save the changes? (y,n) "
+    read INPUT1
+    if [[ "$INPUT1" =~ [yY] ]]; then
+        cp $TMPFILE $FILE
+        echo "Changes saved to $FILE."
+    else
+        echo "Changes not saved."
+    fi
+}
+
+quit () {
+    exec 3>&-
+    rm "$TMPFILE"
+    echo
+    echo "Thank you for using Business Hall!" 
+}
+
 perform_action () {
     case "$1" in
 
         1)
-            # Select business file
-            echo "Enter the path to your selected business file"
-            read FILE_LOCATION
-            if test -f "$FILE_LOCATION"; then
-                echo
-                echo "File selected!"
-                FILE=$FILE_LOCATION
-            else
-                echo
-                echo "File does not exist. Default file selected"
-                FILE="./Businesses.csv"
-            fi
-            cp $FILE $TMPFILE
+            select_file
             ;;
         2)
-            # Print business data
             check_file
             RESULT=$?
             if test "$RESULT" -eq 1; then
-                echo "Enter business code"
-                echo
-                read BUSINESS_CODE
-                echo
-                awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
-                echo
+                print_business_data
             fi
             ;;
         3)
-            # Update business data field
             check_file
             RESULT=$?
             if test "$RESULT" -eq 1; then
-                echo "Enter business code"
-                echo
-                read BUSINESS_CODE
-                echo
-                echo "Old data"
-                echo
-                awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
-                echo
-                awk -v x="$BUSINESS_CODE" 'BEGIN {FS=OFS=","} $1==x {$2="Starbucks"} 1' $TMPFILE > tmp && mv tmp $TMPFILE
-                echo "New data"
-                echo
-                awk -F"," -v x="$BUSINESS_CODE" '$1==x {print}' $TMPFILE | column -s "," -t -N ID,BusinessName,Adress,City,PostCode,Longitude,Latitude
-                echo
+                update_data
             fi
             ;;
         4)
-            # Print entire file
             check_file
             RESULT=$?
             if test "$RESULT" -eq 1 ; then 
-                echo
-                cat $TMPFILE | column -s"," -t | more -df
+                print_entire_file
             fi
             ;;
         5)
-            # Save changes 
             check_file
             RESULT=$?
             if test "$RESULT" -eq 1; then
-                echo -n "Are you sure to want to save the changes? (y,n)"
-                read INPUT1
-                if [[ "$INPUT1" =~ [yY] ]]; then
-                    cp $TMPFILE $FILE
-                    echo "Changes saved in $FILE."
-                else
-                    echo "Changes not saved."
-                fi
+                save_changes
             fi
             ;;
         6)
-            exec 3>&-
-            rm "$TMPFILE"
-            echo
-            echo "Thank you for using Business Hall!" 
+            quit
             ;;
         *)
             echo "Invalid input."
